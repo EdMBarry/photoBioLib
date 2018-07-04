116f06e12a79d704923bf2992bd31e1808e8ca93
Ed Barry <ed.m.barry@gmail.com>
2018-07-04 12:43:13 +1000

    change dict to must_read

--- photoBioModels/photoBioModel/photoBioModelNew.C
+++ photoBioModels/photoBioModel/photoBioModelNew.C
@@ -52,7 +52,7 @@ autoPtr<photoBioModel> photoBioModel::New
                 "photoBioProperties",
                 Enu.time().constant(),
                 Enu.mesh(),
-                IOobject::NO_READ,
+                IOobject::MUST_READ,
                 IOobject::NO_WRITE,
                 false
             )
